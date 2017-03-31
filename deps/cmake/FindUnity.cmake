f10b8088e4496c624f43114d584c0d7c2581b6bd
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-03-31 18:58:07 -0400

    New cmake module to find Unity
    Not implemented for linux, not tested for windows.
    
    Drive-by fix: the cmake module path is now set properly so we can type "cmake /path/to/FbxSharp"

---
+++ deps/cmake/FindUnity.cmake
@@ -0,0 +1,17 @@
+# Look for the executable to find the Unity editor executable.
+# If UNITY_EXECUTABLE_PATH we use it, otherwise we set it.
+
+# Platform-specific code.
+if (NOT DEFINED UNITY_EDITOR_PATH)
+    if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+      list(APPEND UNITY_EXECUTABLE_PATHS "/Applications/Unity/Unity.app/Contents/MacOS/")
+    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+      list(APPEND UNITY_EXECUTABLE_PATHS "c:/Program Files/Unity/")
+    endif()
+
+    find_program(UNITY_EDITOR_PATH Unity PATHS ${UNITY_EXECUTABLE_PATHS})
+endif()
+
+# Standard code to report whether we found the package or not.
+#include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(UNITY DEFAULT_MSG UNITY_EDITOR_PATH)
