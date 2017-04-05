e45690f5c3734b2feeaaec8b6e0d3056c3f9262a
vkovec <viktoria.kovecses@gmail.com>
2017-04-05 16:08:41 -0400

    include only necessary functions from FbxIOSettings
    Also wrote unit + performance tests for FbxIOSettings functions

--- src/fbxiosettings.i
+++ src/fbxiosettings.i
@@ -1,2 +1,12 @@
-%include "fbxsdk/fileio/fbxiosettings.h"
+%ignore __declspec(deprecated);
 
+//%rename($ignore, %isvariable) "";
+//%rename($ignore, %ismember) "";
+
+%ignore ""; // ignore everything
+
+// don't ignore these
+%rename("%s") FbxIOSettings;
+%rename("%s") FbxIOSettings::Create(FbxManager *pManager, const char *pName);
+
+%include "fbxsdk/fileio/fbxiosettings.h"
---
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxIOSettingsTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using FbxSdk;
+
+public class FbxIOSettingsTest {
+
+	[Test]
+	public void TestCreate()
+	{
+		FbxManager manager = FbxManager.Create ();
+		FbxIOSettings ioSettings = FbxIOSettings.Create (manager, "");
+
+		Assert.IsNotNull (ioSettings);
+		Assert.IsInstanceOf<FbxObject> (ioSettings);
+
+		manager.Destroy ();
+	}
+}
