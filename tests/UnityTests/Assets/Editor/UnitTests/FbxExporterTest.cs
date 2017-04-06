7da2563d6be46d070f268822f839ded7047ea55f
vkovec <viktoria.kovecses@gmail.com>
2017-04-06 13:39:32 -0400

    add some FbxExporter unit tests

---
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxExporterTest.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using FbxSdk;
+using System.IO;
+
+namespace UnitTests
+{
+
+    public class FbxExporterTest
+    {
+
+
+        public void ExportHelper (FbxManager manager, FbxDocument doc)
+        {
+            FbxExporter exporter = FbxExporter.Create (manager, "exporter");
+
+            Assert.IsNotNull (exporter);
+
+            string filename = "test.fbx";
+
+            // Initialize the exporter.
+            bool exportStatus = exporter.Initialize (filename, -1, manager.GetIOSettings ());
+
+            Assert.IsTrue (exportStatus);
+
+            exporter.Export (doc);
+
+            Assert.IsTrue (File.Exists (filename));
+
+            exporter.Destroy ();
+
+            // Delete the file once the test is complete
+            File.Delete (filename);
+        }
+
+        [Test]
+        public void TestExportEmptyFbxDocument ()
+        {
+            FbxManager manager = FbxManager.Create ();
+            FbxDocument emptyDoc = FbxDocument.Create (manager, "empty");
+
+            ExportHelper (manager, emptyDoc);
+
+            manager.Destroy ();
+        }
+
+        [Test]
+        public void TestExportNull ()
+        {
+            FbxManager manager = FbxManager.Create ();
+
+            ExportHelper (manager, null);
+
+            manager.Destroy ();
+        }
+
+        [Test]
+        [ExpectedException (typeof(System.ArgumentNullException))]
+        public void TestDestroy ()
+        {
+            FbxManager manager = FbxManager.Create ();
+            FbxExporter exporter = FbxExporter.Create (manager, "exporter");
+
+            exporter.Destroy ();
+
+            exporter.GetName ();
+
+            manager.Destroy ();
+        }
+
+        [Test]
+        [ExpectedException (typeof(System.ArgumentNullException))]
+        public void TestDestroyManager ()
+        {
+            FbxManager manager = FbxManager.Create ();
+            FbxExporter exporter = FbxExporter.Create (manager, "exporter");
+
+            manager.Destroy ();
+            exporter.GetName ();
+        }
+    }
+}
