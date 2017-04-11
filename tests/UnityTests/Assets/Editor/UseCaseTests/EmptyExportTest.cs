a6568382cd10f4445db1489931bf72f931c05b45
sinwood <simon.cf.inwood@gmail.com>
2017-04-11 13:53:06 -0400

    Uni-12954 example export empty scene

--- tests/UnityTests/Assets/Editor/UseCaseTests/EmptyExportTest.cs
+++ tests/UnityTests/Assets/Editor/UseCaseTests/EmptyExportTest.cs
@@ -1,74 +1,159 @@
-﻿using UnityEngine;
+﻿// ***********************************************************************
+// Copyright (c) 2017 Unity Technologies. All rights reserved.  
+//
+// Licensed under the ##LICENSENAME##. 
+// See LICENSE.md file in the project root for full license information.
+// ***********************************************************************
+ 
+using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
 using FbxSdk;
 using System.IO;
-
+using System.Collections.Generic;
+            
 namespace UseCaseTests
 {
 
     public class EmptyExportTest
     {
+        protected string fileNamePrefix { get { return "safe_to_delete_"; } }
+
+        private static Dictionary<string, string> m_dataValues = null;
+
+        protected Dictionary<string, string> dataValues {
+            get {
+                if (m_dataValues == null) {
+                    m_dataValues = new Dictionary<string, string> ()
+                    {
+                        { "title",      "Empty scene" },
+                        { "subject",    "Example of an empty scene with document information settings" },
+                        { "author",     "Unit Technologies" },
+                        { "revision",   "1.0" },
+                        { "keywords",   "example empty scene" },
+                        { "comment",    "basic scene settings. Note that the scene thumnail is not set." },
+                    };
+                }
+                return m_dataValues;
+            }
+        }
+
+        // Declare the path and filename for the scene to be exported.
+        // In this case, the file will be in the same directory as the executable.
+        private string m_fileName = null;
+
+        protected string fileName {
+        	get {
+                if (m_fileName==null) {
+                    m_fileName = this.fileNamePrefix + Path.GetRandomFileName ();
+                }
+        		return m_fileName;
+        	}
+        }
+
+        [SetUp]
+        public virtual void Init ()
+        {
+            foreach (string file in Directory.GetFiles (".", this.fileNamePrefix + "*.fbx")) {
+                File.Delete (file);
+            }
+        }
+
+        [TearDown]
+        public virtual void Term ()
+        {
+            // Delete the file once the test is complete
+            File.Delete (fileName);
+        }
+
+        private FbxScene CreateScene(FbxManager manager)
+        {
+            FbxScene scene = FbxScene.Create (manager, "myScene");
+
+            // create scene info
+            FbxDocumentInfo sceneInfo = FbxDocumentInfo.Create (manager, "mySceneInfo");
+
+            sceneInfo.mTitle      = dataValues["title"];
+            sceneInfo.mSubject    = dataValues["subject"];
+            sceneInfo.mAuthor     = dataValues["author"];
+            sceneInfo.mRevision   = dataValues["revision"];
+            sceneInfo.mKeywords   = dataValues["keywords"];
+            sceneInfo.mComment    = dataValues["comment"];
+
+            scene.SetSceneInfo (sceneInfo);
+
+            // TODO: port SetSceneThumbnail
+
+            return scene;
+        }
+
+        private void CheckSceneInfo(FbxScene scene, Dictionary<string,string> values)
+        {
+            FbxDocumentInfo sceneInfo = scene.GetSceneInfo ();
+
+            Assert.Equals (sceneInfo.mTitle,    values["title"]);
+            Assert.Equals (sceneInfo.mSubject,  values["subject"]);
+            Assert.Equals (sceneInfo.mAuthor,   values["author"]);
+            Assert.Equals (sceneInfo.mRevision, values["revision"]);
+            Assert.Equals (sceneInfo.mKeywords, values["keywords"]);
+            Assert.Equals (sceneInfo.mComment,  values["comment"]);
+        }
 
         [Test]
         public void EditorTest ()
         {
             // Create the FBX SDK manager
-            FbxManager lSdkManager = FbxManager.Create ();
+            using (FbxManager manager = FbxManager.Create ()) {
+                
+                // Create an IOSettings object.
+                FbxIOSettings iosettings = FbxIOSettings.Create (manager, Globals.IOSROOT);
+                manager.SetIOSettings (iosettings);
 
-            // Create an IOSettings object.
-            FbxIOSettings ios = FbxIOSettings.Create (lSdkManager, Globals.IOSROOT);
-            lSdkManager.SetIOSettings (ios);
+                // Export the scene
+                using (FbxExporter exporter = FbxExporter.Create (manager, "myExporter")) {
 
-            // Create an exporter.
-            FbxExporter lExporter = FbxExporter.Create (lSdkManager, "");
+                    // Initialize the exporter.
+                    bool status = exporter.Initialize (this.fileName, -1, manager.GetIOSettings ());
 
-            // Declare the path and filename of the file to which the scene will be exported.
-            // In this case, the file will be in the same directory as the executable.
-            string lFilename = "test.fbx";
+                    // Check that export status is True
+                    Assert.IsTrue (status);
 
-            // Initialize the exporter.
-            bool lExportStatus = lExporter.Initialize (lFilename, -1, lSdkManager.GetIOSettings ());
+                    // Create a new scene so it can be populated by the imported file.
+                    FbxScene scene = CreateScene (manager);
 
-            // Check that export status is True
-            Assert.IsTrue (lExportStatus);
+                    CheckSceneInfo (scene, this.dataValues);
 
-            // Create a new scene so it can be populated by the imported file.
-            FbxScene lScene = FbxScene.Create (lSdkManager, "myScene");
+                    // Export the scene to the file.
+                    exporter.Export (scene);
 
-            // Export the scene to the file.
-            lExporter.Export (lScene);
+                    // Check if file exists
+                    Assert.IsTrue (File.Exists (fileName));
 
-            // Check if file exists
-            Assert.IsTrue (File.Exists (lFilename));
+                    exporter.Destroy ();
+                }
 
-            lExporter.Destroy ();
+                // Import the scene to make sure file is valid
+                using (FbxImporter importer = FbxImporter.Create (manager, "myImporter")) {
 
-            // Import to make sure file is valid
+                    // Initialize the importer.
+                    bool status = importer.Initialize (this.fileName, -1, manager.GetIOSettings ());
 
-            // Create an importer.
-            FbxImporter lImporter = FbxImporter.Create (lSdkManager, "");
+                    Assert.IsTrue (status);
 
-            // Initialize the importer.
-            bool lImportStatus = lImporter.Initialize (lFilename, -1, lSdkManager.GetIOSettings ());
+                    // Create a new scene so it can be populated by the imported file.
+                    FbxScene scene = FbxScene.Create (manager, "myScene");
 
-            Assert.IsTrue (lImportStatus);
+                    // Import the contents of the file into the scene.
+                    importer.Import (scene);
 
-            // Create a new scene so it can be populated by the imported file.
-            FbxScene newScene = FbxScene.Create (lSdkManager, "myScene2");
+                    // check that the scene is valid
+                    CheckSceneInfo (scene, this.dataValues);
 
-            // Import the contents of the file into the scene.
-            lImporter.Import (newScene);
+                    importer.Destroy ();
+                }
 
-            lImporter.Destroy ();
-
-            // check that the scene is valid
-            Assert.GreaterOrEqual (newScene.GetNodeCount (), 0);
-
-            lSdkManager.Destroy ();
-
-            // Delete the file once the test is complete
-            File.Delete (lFilename);
+                manager.Destroy ();
+            }
         }
     }
 }
