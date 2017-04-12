9aab14dcbc6c4cbb3555a827fabcf90456c1e1db
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-04-12 13:00:50 -0400

    Per review comment, generalize the fix-header cmake hackery.
    Moved it to UseFixHeaders into a nice macro.

---
+++ deps/cmake/UseFixHeaders.cmake
@@ -0,0 +1,13 @@
+macro(SWIG_FIX_HEADER swigmodule inputheader)
+    get_filename_component(basename ${inputheader} NAME)
+    set(fix_header_output_dir "${CMAKE_BINARY_DIR}/${swigmodule}-fixed-headers")
+    set(fix_header_output "${fix_header_output_dir}/${basename}")
+    list(APPEND SWIG_MODULE_${swigmodule}_EXTRA_DEPS ${fix_header_output})
+    add_custom_command(
+        OUTPUT ${fix_header_output}
+        COMMAND ${PYTHON_EXECUTABLE}
+        ARGS "${CMAKE_SOURCE_DIR}/scripts/fix-headers.py" "${fix_header_output_dir}" "${inputheader}"
+        MAIN_DEPENDENCY ${inputheader}
+        DEPENDENCY "${CMAKE_SOURCE_DIR}/scripts/fix-headers.py"
+    )
+endmacro()
