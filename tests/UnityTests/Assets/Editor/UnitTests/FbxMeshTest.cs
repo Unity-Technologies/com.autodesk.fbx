9ae394262e81dfbcf7f3c5596af1907216fa5570
vkovec <viktoria.kovecses@gmail.com>
2017-04-18 15:04:59 -0400

    Added test for FbxMesh
    - Note: won't compile until all the missing parent classes are also
    included (FbxGeometry, FbxGeometryBase, etc.)

---
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxMeshTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using FbxSdk;
+
+namespace UnitTests
+{
+    public class FbxMeshTest : Base<FbxMesh>
+    {
+
+        [Test]
+        public void TestCreateFromObject ()
+        {
+            FbxObject obj = CreateObject("object");
+
+            FbxMesh mesh = FbxMesh.Create (obj, "mesh");
+
+            Assert.IsInstanceOf<FbxMesh> (obj);
+        }
+    }
+}
