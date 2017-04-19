1649f7a436107a26445095a049cbe9d274a90127
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-04-19 14:43:18 -0400

    Fix coverage for matrix tests.

--- tests/UnityTests/Assets/Editor/UnitTests/FbxAMatrixTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxAMatrixTest.cs
@@ -25,16 +25,69 @@ namespace UnitTests
                     Assert.AreEqual(x == y ? 1 : 0, mx.Get(y, x));
                 }
             }
-        }
 
-        [Test]
-        public void TestUsing ()
-        {
-            /* make sure that the using form compiles and doesn't crash */
+            //////
+            // Tests for the inherited Double4x4
+
+            // make sure the no-arg constructor doesn't crash
+            new FbxAMatrix();
+
+            // make sure we can dispose
             using (new FbxAMatrix()) { }
-
-            // Make sure we can explicitly dispose as well.
             new FbxAMatrix().Dispose();
+
+            // make sure equality works.
+            Assert.IsTrue(new FbxAMatrix().Equals(new FbxAMatrix()));
+
+            Assert.IsTrue(new FbxAMatrix() == new FbxAMatrix());
+            Assert.IsFalse(new FbxAMatrix() != new FbxAMatrix());
+
+            Assert.IsFalse(new FbxAMatrix() == (FbxAMatrix)null);
+            Assert.IsTrue(new FbxAMatrix() != (FbxAMatrix)null);
+
+            Assert.IsFalse((FbxAMatrix)null == new FbxAMatrix());
+            Assert.IsTrue((FbxAMatrix)null != new FbxAMatrix());
+
+            Assert.IsTrue((FbxAMatrix)null == (FbxAMatrix)null);
+            Assert.IsFalse((FbxAMatrix)null != (FbxAMatrix)null);
+
+            // Test operator[]
+            var v = new FbxAMatrix();
+            var a = new FbxDouble4(1,2,3,4);
+            var b = new FbxDouble4(5,6,7,8);
+            var c = new FbxDouble4(9,8,7,6);
+            var d = new FbxDouble4(5,4,3,2);
+            v[0] = a;
+            Assert.AreEqual(a.X, v[0].X);
+            Assert.AreEqual(a.Y, v[0].Y);
+            Assert.AreEqual(a.Z, v[0].Z);
+            Assert.AreEqual(a.W, v[0].W);
+            Assert.AreEqual(a, v[0]);
+            v[1] = b;
+            Assert.AreEqual(b, v[1]);
+            v[2] = c;
+            Assert.AreEqual(c, v[2]);
+            v[3] = d;
+            Assert.AreEqual(d, v[3]);
+            Assert.That(() => v[-1], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+            Assert.That(() => v[ 4], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+            Assert.That(() => v[-1] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+            Assert.That(() => v[ 4] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
+
+            // Test members W/X/Y/Z
+            v = mx;
+            Assert.AreEqual(a, v.X);
+            Assert.AreEqual(b, v.Y);
+            Assert.AreEqual(c, v.Z);
+            Assert.AreEqual(d, v.W);
+            v.X = d;
+            v.Y = c;
+            v.Z = b;
+            v.W = a;
+            Assert.AreEqual(d, v.X);
+            Assert.AreEqual(c, v.Y);
+            Assert.AreEqual(b, v.Z);
+            Assert.AreEqual(a, v.W);
         }
 
 #if ENABLE_COVERAGE_TEST
