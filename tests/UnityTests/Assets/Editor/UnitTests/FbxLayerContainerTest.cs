5b20c6a772cc8836f36cd1634b57f45413e7e8fd
vkovec <viktoria.kovecses@gmail.com>
2017-04-24 15:46:25 -0400

    added unit tests

---
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxLayerContainerTest.cs
@@ -0,0 +1,42 @@
+﻿// ***********************************************************************
+// Copyright (c) 2017 Unity Technologies. All rights reserved.  
+//
+// Licensed under the ##LICENSENAME##. 
+// See LICENSE.md file in the project root for full license information.
+// ***********************************************************************
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using FbxSdk;
+
+namespace UnitTests
+{
+    public class FbxLayerContainerTest : Base<FbxLayerContainer>
+    {
+
+        [Test]
+        public void TestCreateLayer()
+        {
+            using (FbxLayerContainer layerContainer = CreateObject ("layerContainer")) {
+                int index = layerContainer.CreateLayer ();
+                Assert.GreaterOrEqual (index, -1); // check an index is returned (-1 is error)
+            }
+        }
+
+        [Test]
+        public void TestGetLayer()
+        {
+            using (FbxLayerContainer layerContainer = CreateObject ("layerContainer")) {
+                int index = layerContainer.CreateLayer ();
+                Assert.GreaterOrEqual (index, 0); // check we created a valid layer
+
+                // make sure doesn't crash and returns expected value
+                Assert.IsNotNull (layerContainer.GetLayer (index));
+                Assert.IsNull (layerContainer.GetLayer (int.MinValue));
+                Assert.IsNull (layerContainer.GetLayer (int.MaxValue));
+            }
+        }
+    }
+}
