459dbda9483cb12eaa2882c9a4bf20eb601061f6
vkovec <viktoria.kovecses@gmail.com>
2017-04-27 14:26:12 -0400

    make the visibility example also export the mesh, so it's more visual

--- examples/export/Assets/Editor/FbxExporter08.cs
+++ examples/export/Assets/Editor/FbxExporter08.cs
@@ -5,6 +5,8 @@
 // See LICENSE.md file in the project root for full license information.
 // ***********************************************************************
 
+#define UNI_16194
+
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,7 +21,7 @@ namespace FbxSdk.Examples
         public class FbxExporter08 : System.IDisposable
         {
             const string Title = 
-                "Example 08: exporting a node hierarchy with transforms and visibility";
+                "Example 08: exporting a node hierarchy with visibility";
             
             const string Subject = 
                 @"Example FbxExporter08 illustrates how to:
@@ -27,12 +29,13 @@ namespace FbxSdk.Examples
                     2) create a scene                           
                     3) create a hierarchy of nodes              
                     4) add transform data to each node
-                    5) set the visibility of nodes and hierarchies          
-                    6) export the nodes to a FBX file (ASCII mode)
+                    5) add static mesh data to each node
+                    6) set the visibility of nodes and hierarchies          
+                    7) export the nodes to a FBX file (ASCII mode)
                 ";
             
             const string Keywords = 
-                "export node transform visibility";
+                "export node visibility";
             
             const string Comments = 
                 @"In this example we set the root node invisible, but leave its children visible.";
@@ -56,6 +59,7 @@ namespace FbxSdk.Examples
                 NumNodes++;
 
                 ExportTransform ( unityGo.transform, fbxNode);
+                ExportMesh (GetMeshInfo (unityGo), fbxNode, fbxScene);
 #if UNI_16194
                 // if this GameObject is at the root of the scene,
                 // make it invisible, but leave all its children as is
@@ -103,6 +107,50 @@ namespace FbxSdk.Examples
                 fbxNode.LclScaling.Set(fbxScale);
 
                 return;
+            }
+
+            /// <summary>
+            /// Unconditionally export this mesh object to the file.
+            /// We have decided; this mesh is definitely getting exported.
+            /// </summary>
+            public void ExportMesh (MeshInfo mesh, FbxNode fbxNode, FbxScene fbxScene)
+            {
+                if (!mesh.IsValid)
+                    return;
+
+                NumMeshes++;
+                NumTriangles += mesh.Triangles.Length / 3;
+                NumVertices += mesh.VertexCount;
+
+                // create the mesh structure.
+                FbxMesh fbxMesh = FbxMesh.Create (fbxScene, MakeObjectName ("Scene"));
+
+                // Create control points.
+                int NumControlPoints = mesh.VertexCount;
+                fbxMesh.InitControlPoints (NumControlPoints);
+
+                // copy control point data from Unity to FBX
+                for (int v = 0; v < NumControlPoints; v++)
+                {
+                    fbxMesh.SetControlPointAt(new FbxVector4(mesh.Vertices[v].x, mesh.Vertices[v].y, mesh.Vertices[v].z), v);
+                }
+
+                /* 
+                 * Create polygons after FbxGeometryElementMaterial are created. 
+                 * TODO: Assign material indices.
+                 */
+                int vId = 0;
+                for (int f = 0; f < mesh.Triangles.Length / 3; f++) {
+                    fbxMesh.BeginPolygon ();
+                    fbxMesh.AddPolygon (mesh.Triangles [vId++]);
+                    fbxMesh.AddPolygon (mesh.Triangles [vId++]);
+                    fbxMesh.AddPolygon (mesh.Triangles [vId++]);
+                    fbxMesh.EndPolygon ();
+                }
+
+                // set the fbxNode containing the mesh
+                fbxNode.SetNodeAttribute (fbxMesh);
+                fbxNode.SetShadingMode (FbxNode.EShadingMode.eWireFrame);
             }
 
             /// <summary>
@@ -198,6 +246,127 @@ namespace FbxSdk.Examples
             {
                 return Selection.activeTransform != null;
             }
+
+            //
+            // export mesh info from Unity
+            //
+            ///<summary>
+            ///Information about the mesh that is important for exporting. 
+            ///</summary>
+            public struct MeshInfo
+            {
+                /// <summary>
+                /// The transform of the mesh.
+                /// </summary>
+                public Matrix4x4 xform;
+                public Mesh mesh;
+
+                /// <summary>
+                /// The gameobject in the scene to which this mesh is attached.
+                /// This can be null: don't rely on it existing!
+                /// </summary>
+                public GameObject unityObject;
+
+                /// <summary>
+                /// Return true if there's a valid mesh information
+                /// </summary>
+                /// <value>The vertex count.</value>
+                public bool IsValid { get { return mesh != null; } }
+
+                /// <summary>
+                /// Gets the vertex count.
+                /// </summary>
+                /// <value>The vertex count.</value>
+                public int VertexCount { get { return mesh.vertexCount; } }
+
+                /// <summary>
+                /// Gets the triangles. Each triangle is represented as 3 indices from the vertices array.
+                /// Ex: if triangles = [3,4,2], then we have one triangle with vertices vertices[3], vertices[4], and vertices[2]
+                /// </summary>
+                /// <value>The triangles.</value>
+                public int [] Triangles { get { return mesh.triangles; } }
+
+                /// <summary>
+                /// Gets the vertices, represented in local coordinates.
+                /// </summary>
+                /// <value>The vertices.</value>
+                public Vector3 [] Vertices { get { return mesh.vertices; } }
+
+                /// <summary>
+                /// Gets the normals for the vertices.
+                /// </summary>
+                /// <value>The normals.</value>
+                public Vector3 [] Normals { get { return mesh.normals; } }
+
+                /// <summary>
+                /// Gets the uvs.
+                /// </summary>
+                /// <value>The uv.</value>
+                public Vector2 [] UV { get { return mesh.uv; } }
+
+                /// <summary>
+                /// Initializes a new instance of the <see cref="MeshInfo"/> struct.
+                /// </summary>
+                /// <param name="mesh">A mesh we want to export</param>
+                public MeshInfo (Mesh mesh)
+                {
+                    this.mesh = mesh;
+                    this.xform = Matrix4x4.identity;
+                    this.unityObject = null;
+                }
+
+                /// <summary>
+                /// Initializes a new instance of the <see cref="MeshInfo"/> struct.
+                /// </summary>
+                /// <param name="gameObject">The GameObject the mesh is attached to.</param>
+                /// <param name="mesh">A mesh we want to export</param>
+                public MeshInfo (GameObject gameObject, Mesh mesh)
+                {
+                    this.mesh = mesh;
+                    this.xform = gameObject.transform.localToWorldMatrix;
+                    this.unityObject = gameObject;
+                }
+            }
+
+            /// <summary>
+            /// Get a mesh renderer's mesh.
+            /// </summary>
+            private MeshInfo GetMeshInfo (GameObject gameObject, bool requireRenderer = true)
+            {
+                if (requireRenderer) {
+                    // Verify that we are rendering. Otherwise, don't export.
+                    var renderer = gameObject.gameObject.GetComponent<MeshRenderer> ();
+                    if (!renderer || !renderer.enabled) {
+                        return new MeshInfo ();
+                    }
+                }
+
+                var meshFilter = gameObject.GetComponent<MeshFilter> ();
+                if (!meshFilter) {
+                    return new MeshInfo ();
+                }
+                var mesh = meshFilter.sharedMesh;
+                if (!mesh) {
+                    return new MeshInfo ();
+                }
+
+                return new MeshInfo (gameObject, mesh);
+            }
+
+            /// <summary>
+            /// Number of meshes exported
+            /// </summary>
+            public int NumMeshes { private set; get; }
+
+            /// <summary>
+            /// Number of triangles exported
+            /// </summary>
+            public int NumTriangles { private set; get; }
+
+            /// <summary>
+            /// Number of vertices
+            /// </summary>
+            public int NumVertices { private set; get; }
 
             /// <summary>
             /// Number of nodes exported including siblings and decendents
