0dfba2bf788a301b121292b9678c5cec7d85053f
vkovec <viktoria.kovecses@gmail.com>
2017-04-27 15:03:19 -0400

    added tests for SetVisibility and VisibilityInheritance

--- tests/UnityTests/Assets/Editor/UnitTests/FbxPropertyTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxPropertyTest.cs
@@ -11,6 +11,18 @@ namespace UnitTests
 {
     public class FbxPropertyTest
     {
+        // tests that should work for any subclass of FbxProperty
+        private void GenericPropertyTests<T>(T property, FbxNode node, string propertyName) where T:FbxProperty{
+            Assert.AreEqual(propertyName, property.GetName());
+            Assert.AreEqual(propertyName, property.ToString());
+            Assert.AreEqual(propertyName, property.GetHierarchicalName());
+            Assert.AreEqual(propertyName, property.GetLabel(true));
+            property.SetLabel("label");
+            Assert.AreEqual("label", property.GetLabel());
+            Assert.AreEqual(node, property.GetFbxObject());
+            Assert.AreEqual(property.GetFbxObject(), node); // test it both ways just in case equals is busted
+        }
+
         [Test]
         public void BasicTests ()
         {
@@ -18,14 +30,7 @@ namespace UnitTests
             using(var manager = FbxManager.Create()) {
                 var node = FbxNode.Create(manager, "node");
                 var property = node.LclTranslation;
-                Assert.AreEqual("Lcl Translation", property.GetName());
-                Assert.AreEqual("Lcl Translation", property.ToString());
-                Assert.AreEqual("Lcl Translation", property.GetHierarchicalName());
-                Assert.AreEqual("Lcl Translation", property.GetLabel(true));
-                property.SetLabel("label");
-                Assert.AreEqual("label", property.GetLabel());
-                Assert.AreEqual(node, property.GetFbxObject());
-                Assert.AreEqual(property.GetFbxObject(), node); // test it both ways just in case equals is busted
+                GenericPropertyTests<FbxPropertyDouble3> (property, node, "Lcl Translation");
 
                 var dbl3 = property.Get();
                 Assert.AreEqual(new FbxDouble3(), dbl3);
@@ -37,6 +42,21 @@ namespace UnitTests
                 // modify the test then.
                 property.Dispose();
                 using(var prop2 = node.LclScaling) { }
+
+                // Run the same tests for FbxPropertyT<FbxBool> used by VisibilityInheritance
+                var visibilityInheritance = node.VisibilityInheritance;
+                GenericPropertyTests<FbxPropertyBool> (visibilityInheritance, node, "Visibility Inheritance");
+
+                var fbxBool = visibilityInheritance.Get();
+                Assert.AreEqual(true, fbxBool); // VisibilityInheritance is true by default
+                visibilityInheritance.Set(false);
+                fbxBool = visibilityInheritance.Get();
+                Assert.AreEqual(false, fbxBool);
+
+                // TODO: dispose will in the future destroy, which is illegal in this context;
+                // modify the test then.
+                visibilityInheritance.Dispose();
+                using(var prop2 = node.VisibilityInheritance) { }
             }
         }
 
@@ -45,6 +65,7 @@ namespace UnitTests
         public void TestCoverage() {
             CoverageTester.TestCoverage(typeof(FbxProperty), this.GetType());
             CoverageTester.TestCoverage(typeof(FbxPropertyDouble3), this.GetType());
+            CoverageTester.TestCoverage(typeof(FbxPropertyBool), this.GetType());
         }
 #endif
     }
