662aba1d96dbe60e56fa103dd85e15c4ddb08b36
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-04-28 18:05:57 -0400

    Scripted up generation of %fbximmutable lists.
    Generalized from being just for datatypes; now it's easy to add more.
    
    This was in support of FbxTexture.

--- deps/cmake/RunPythonScript.cmake
+++ deps/cmake/RunPythonScript.cmake
@@ -30,3 +30,21 @@ macro(FBXSHARP_RUN_PYTHON)
   )
   list(APPEND ${_runpy_TARGETDEPS} ${_runpy_OUTPUT})
 endmacro()
+
+
+# fbxsharp_discover_immutables(OUTPUT foo.i
+#       HEADERS bar.h quux.h
+#       TARGETDEPS SWIG_MODULE_${module}_EXTRA_DEPS)
+macro(FBXSHARP_DISCOVER_IMMUTABLES)
+  cmake_parse_arguments(_immutables
+        ""
+        "OUTPUT;TARGETDEPS"
+        "HEADERS"
+        ${ARGN})
+  fbxsharp_run_python(OUTPUT ${_immutables_OUTPUT}
+        SCRIPT ${CMAKE_SOURCE_DIR}/scripts/discover-immutables.py
+        DEPENDS ${_immutables_HEADERS}
+        ARGS ${_immutables_OUTPUT} ${_immutables_HEADERS}
+        TARGETDEPS ${_immutables_TARGETDEPS}
+  )
+endmacro()
