26c35d310ea067b90670fa9801f6aa07c2825680
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-04-28 09:52:20 -0400

    Better menu item name.

--- examples/export/Assets/Editor/FbxExporter06.cs
+++ examples/export/Assets/Editor/FbxExporter06.cs
@@ -40,7 +40,7 @@ namespace FbxSdk.Examples
             const string Comments =
                 @"";
 
-            const string MenuItemName = "File/Export/Export (static mesh with specular materials and textures) to FBX";
+            const string MenuItemName = "File/Export/Export (static mesh with materials) to FBX";
 
             const string FileBaseName = "example_static_mesh_with_materials_and_textures";
 
