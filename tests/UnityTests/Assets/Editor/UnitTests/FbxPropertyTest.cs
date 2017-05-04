1a2e113934cd49bde3596593e3bec445f83dc1f5
vkovec <viktoria.kovecses@gmail.com>
2017-05-04 15:18:22 -0400

    add unit tests

--- tests/UnityTests/Assets/Editor/UnitTests/FbxPropertyTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxPropertyTest.cs
@@ -79,6 +79,22 @@ namespace UnitTests
             Assert.AreEqual(parent, property.GetFbxObject());
             Assert.AreEqual(property.GetFbxObject(), parent); // test it both ways just in case equals is busted
 
+            // test GetCurveNode() (make sure it doesn't crash)
+            FbxAnimCurveNode curveNode = property.GetCurveNode();
+            Assert.IsNull (curveNode); // didn't create one so should be null
+
+            curveNode = property.GetCurveNode (true);
+            // TODO: figure out why the curve node doesn't get created
+            //Assert.IsNotNull (curveNode);
+
+            property.GetCurveNode (FbxAnimStack.Create (parent, "anim stack"));
+            property.GetCurveNode (FbxAnimStack.Create (parent, "anim stack"), true);
+            property.GetCurveNode (FbxAnimLayer.Create (parent, "anim layer"));
+            property.GetCurveNode (FbxAnimLayer.Create (parent, "anim layer"), true);
+
+            Assert.That (() => { property.GetCurveNode((FbxAnimStack)null); }, Throws.Exception.TypeOf<System.NullReferenceException>());
+            Assert.That (() => { property.GetCurveNode((FbxAnimLayer)null); }, Throws.Exception.TypeOf<System.NullReferenceException>());
+
             // verify this in the future: will dispose destroy?
             property.Dispose();
         }
