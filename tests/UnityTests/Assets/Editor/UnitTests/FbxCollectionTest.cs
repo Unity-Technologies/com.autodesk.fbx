8602eb8f88a2193e67850805ddd5a523f4b43c2f
vkovec <viktoria.kovecses@gmail.com>
2017-05-04 10:40:54 -0400

    add missing unit tests

--- tests/UnityTests/Assets/Editor/UnitTests/FbxCollectionTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxCollectionTest.cs
@@ -1,9 +1,12 @@
-﻿// ***********************************************************************
-// Copyright (c) 2017 Unity Technologies. All rights reserved.
+// ***********************************************************************
+// Copyright (c) 2017 Unity Technologies. All rights reserved.  
 //
-// Licensed under the ##LICENSENAME##.
+// Licensed under the ##LICENSENAME##. 
 // See LICENSE.md file in the project root for full license information.
 // ***********************************************************************
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 using FbxSdk;
@@ -12,11 +15,27 @@ namespace UnitTests
 {
     public class FbxCollectionTest : Base<FbxCollection>
     {
+
+        public static void GenericTests<T>(T fbxCollection, FbxManager manager) where T : FbxCollection
+        {
+            // TODO: FbxScene has a member count of 3 instead of one (even after clearing), is this normal?
+            int initialMemberCount = fbxCollection.GetMemberCount ();
+
+            // test AddMember
+            FbxObject obj = FbxObject.Create (manager, "");
+            bool result = fbxCollection.AddMember (obj);
+            Assert.IsTrue (result);
+            Assert.AreEqual(initialMemberCount+1, fbxCollection.GetMemberCount());
+
+            // test Clear
+            fbxCollection.Clear ();
+            Assert.AreEqual (initialMemberCount, fbxCollection.GetMemberCount());
+        }
+
         [Test]
-        public void TestBasics() {
-            // Call all the functions.
-            var collection = CreateObject();
-            collection.Clear();
+        public void TestBasics ()
+        {
+            GenericTests (CreateObject ("fbx collection"), Manager);
         }
     }
 }
