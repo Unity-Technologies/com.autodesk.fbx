cd2179d473da77875503962d32d379cac45e7911
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2017-06-02 21:52:34 -0400

    Refactor double4x4 and matrix tests.

--- tests/UnityTests/Assets/Editor/UnitTests/FbxAMatrixTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxAMatrixTest.cs
@@ -9,13 +9,8 @@ using FbxSdk;
 
 namespace UnitTests
 {
-    public class FbxAMatrixTest
+    public class FbxAMatrixTest : FbxDouble4x4TestBase<FbxAMatrix>
     {
-#if ENABLE_COVERAGE_TEST
-        [Test]
-        public void TestCoverage() { CoverageTester.TestCoverage(typeof(FbxAMatrix), this.GetType()); }
-#endif
-
         [Test]
         public void TestEquality()
         {
@@ -30,79 +25,20 @@ namespace UnitTests
         [Test]
         public void BasicTests ()
         {
+            base.TestElementAccessAndDispose(new FbxAMatrix());
+
             // make sure the constructors compile and don't crash
             new FbxAMatrix();
             new FbxAMatrix(new FbxAMatrix());
             var mx = new FbxAMatrix(new FbxVector4(), new FbxVector4(), new FbxVector4(1,1,1));
 
-            // check that the matrix is the id matrix */
+            // check that the matrix is the id matrix
+            Assert.IsTrue(mx.IsIdentity());
             for(int y = 0; y < 4; ++y) {
                 for(int x = 0; x < 4; ++x) {
                     Assert.AreEqual(x == y ? 1 : 0, mx.Get(y, x));
                 }
             }
-
-            //////
-            // Tests for the inherited Double4x4
-
-            // make sure the no-arg constructor doesn't crash
-            new FbxAMatrix();
-
-            // make sure we can dispose
-            using (new FbxAMatrix()) { }
-            DisposeTester.TestDispose(new FbxAMatrix());
-
-            // make sure equality works.
-            Assert.IsTrue(new FbxAMatrix().Equals(new FbxAMatrix()));
-
-            Assert.IsTrue(new FbxAMatrix() == new FbxAMatrix());
-            Assert.IsFalse(new FbxAMatrix() != new FbxAMatrix());
-
-            Assert.IsFalse(new FbxAMatrix() == (FbxAMatrix)null);
-            Assert.IsTrue(new FbxAMatrix() != (FbxAMatrix)null);
-
-            Assert.IsFalse((FbxAMatrix)null == new FbxAMatrix());
-            Assert.IsTrue((FbxAMatrix)null != new FbxAMatrix());
-
-            Assert.IsTrue((FbxAMatrix)null == (FbxAMatrix)null);
-            Assert.IsFalse((FbxAMatrix)null != (FbxAMatrix)null);
-
-            // Test operator[]
-            var v = new FbxAMatrix();
-            var a = new FbxDouble4(1,2,3,4);
-            var b = new FbxDouble4(5,6,7,8);
-            var c = new FbxDouble4(9,8,7,6);
-            var d = new FbxDouble4(5,4,3,2);
-            v[0] = a;
-            Assert.AreEqual(a.X, v[0].X);
-            Assert.AreEqual(a.Y, v[0].Y);
-            Assert.AreEqual(a.Z, v[0].Z);
-            Assert.AreEqual(a.W, v[0].W);
-            Assert.AreEqual(a, v[0]);
-            v[1] = b;
-            Assert.AreEqual(b, v[1]);
-            v[2] = c;
-            Assert.AreEqual(c, v[2]);
-            v[3] = d;
-            Assert.AreEqual(d, v[3]);
-            Assert.That(() => v[-1], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[ 4], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[-1] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[ 4] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-
-            // Test members W/X/Y/Z
-            Assert.AreEqual(a, v.X);
-            Assert.AreEqual(b, v.Y);
-            Assert.AreEqual(c, v.Z);
-            Assert.AreEqual(d, v.W);
-            v.X = d;
-            v.Y = c;
-            v.Z = b;
-            v.W = a;
-            Assert.AreEqual(d, v.X);
-            Assert.AreEqual(c, v.Y);
-            Assert.AreEqual(b, v.Z);
-            Assert.AreEqual(a, v.W);
         }
     }
 }
--- tests/UnityTests/Assets/Editor/UnitTests/FbxMatrixTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxMatrixTest.cs
@@ -9,13 +9,8 @@ using FbxSdk;
 
 namespace UnitTests
 {
-    public class FbxMatrixTest
+    public class FbxMatrixTest : FbxDouble4x4TestBase<FbxMatrix>
     {
-#if ENABLE_COVERAGE_TEST
-        [Test]
-        public void TestCoverage() { CoverageTester.TestCoverage(typeof(FbxMatrix), this.GetType()); }
-#endif
-
         [Test]
         public void TestEquality()
         {
@@ -30,6 +25,8 @@ namespace UnitTests
         [Test]
         public void BasicTests ()
         {
+            base.TestElementAccessAndDispose(new FbxMatrix());
+
             FbxMatrix mx;
 
             // make sure the constructors compile and don't crash
@@ -37,6 +34,7 @@ namespace UnitTests
             mx = new FbxMatrix(new FbxMatrix());
             mx = new FbxMatrix(new FbxAMatrix());
             mx = new FbxMatrix(new FbxVector4(), new FbxVector4(), new FbxVector4(1,1,1));
+            mx = new FbxMatrix(new FbxVector4(), new FbxQuaternion(), new FbxVector4(1,1,1));
             mx = new FbxMatrix(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
 
             /* Check the values we typed in match up. */
@@ -55,67 +53,8 @@ namespace UnitTests
                 }
             }
 
-            //////
-            // Tests for the inherited Double4x4
-
-            // make sure the no-arg constructor doesn't crash
-            new FbxMatrix();
-
-            // make sure we can dispose
-            using (new FbxMatrix()) { }
-            DisposeTester.TestDispose(new FbxMatrix());
-
-            // make sure equality works.
-            Assert.IsTrue(new FbxMatrix().Equals(new FbxMatrix()));
-
-            Assert.IsTrue(new FbxMatrix() == new FbxMatrix());
-            Assert.IsFalse(new FbxMatrix() != new FbxMatrix());
-
-            Assert.IsFalse(new FbxMatrix() == (FbxMatrix)null);
-            Assert.IsTrue(new FbxMatrix() != (FbxMatrix)null);
-
-            Assert.IsFalse((FbxMatrix)null == new FbxMatrix());
-            Assert.IsTrue((FbxMatrix)null != new FbxMatrix());
-
-            Assert.IsTrue((FbxMatrix)null == (FbxMatrix)null);
-            Assert.IsFalse((FbxMatrix)null != (FbxMatrix)null);
-
-            // Test operator[]
-            var v = new FbxMatrix();
-            var a = new FbxDouble4(1,2,3,4);
-            var b = new FbxDouble4(5,6,7,8);
-            var c = new FbxDouble4(9,8,7,6);
-            var d = new FbxDouble4(5,4,3,2);
-            v[0] = a;
-            Assert.AreEqual(a.X, v[0].X);
-            Assert.AreEqual(a.Y, v[0].Y);
-            Assert.AreEqual(a.Z, v[0].Z);
-            Assert.AreEqual(a.W, v[0].W);
-            Assert.AreEqual(a, v[0]);
-            v[1] = b;
-            Assert.AreEqual(b, v[1]);
-            v[2] = c;
-            Assert.AreEqual(c, v[2]);
-            v[3] = d;
-            Assert.AreEqual(d, v[3]);
-            Assert.That(() => v[-1], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[ 4], Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[-1] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-            Assert.That(() => v[ 4] = a, Throws.Exception.TypeOf<System.IndexOutOfRangeException>());
-
-            // Test 4-argument constructor and members W/X/Y/Z
-            Assert.AreEqual(a, v.X);
-            Assert.AreEqual(b, v.Y);
-            Assert.AreEqual(c, v.Z);
-            Assert.AreEqual(d, v.W);
-            v.X = d;
-            v.Y = c;
-            v.Z = b;
-            v.W = a;
-            Assert.AreEqual(d, v.X);
-            Assert.AreEqual(c, v.Y);
-            Assert.AreEqual(b, v.Z);
-            Assert.AreEqual(a, v.W);
+            /* normal transpose operation */
+            Assert.AreEqual(mx, mx2.Transpose());
 
             // Test getting the elements from a matrix built by TRS
             mx = new FbxMatrix(new FbxVector4(1,2,3), new FbxVector4(0,90,0), new FbxVector4(1,1,1));
