8050e329aaa004d811853c4564c9a617f2135462
sinwood <simon.cf.inwood@gmail.com>
2017-08-04 18:34:46 -0400

    refactor UNITY_EDITOR_DLL_PATH to work w custom install

--- deps/cmake/FindUnity.cmake
+++ deps/cmake/FindUnity.cmake
@@ -11,11 +11,8 @@
 # Platform-specific code.
 if (NOT DEFINED UNITY_EDITOR_PATH)
     if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-      SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
-    
       set(UNITY_PATH "/Applications/Unity")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop.app")
-      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Unity.app/Contents/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Unity.app/Contents/MacOS/")
       
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
@@ -23,28 +20,48 @@ if (NOT DEFINED UNITY_EDITOR_PATH)
       
       set(UNITY_PATH "c:/Program Files/Unity2017.1.0f3")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop")
-      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Editor/Data/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Editor/")
       
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
     
       set(UNITY_PATH "/opt/Unity")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop")
-      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Editor/Data/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Editor/")
       
     endif()
 
     find_program(UNITY_EDITOR_PATH Unity PATHS ${UNITY_EXECUTABLE_PATHS})
 
+    # Standard code to report whether we found the package or not.
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Unity DEFAULT_MSG UNITY_EDITOR_PATH)
+else()
+    message("Using ${UNITY_EDITOR_PATH}")
+endif()
+
+if (NOT DEFINED UNITY_EDITOR_DLL_PATH)
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
+    
+    if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+        get_filename_component(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_PATH}" PATH)
+        get_filename_component(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_DLL_PATH}" DIRECTORY)
+
+        set(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_DLL_PATH}/Managed")
+
+    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+        get_filename_component(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_PATH}" PATH)
+        set(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_DLL_PATH}/Data/Managed")
+
+    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+        get_filename_component(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_PATH}" PATH)
+        set(UNITY_EDITOR_DLL_PATH "${UNITY_EDITOR_DLL_PATH}/Data/Managed")
+    endif()
+
     message("Looking for UnityEditor.dll in ${UNITY_EDITOR_DLL_PATH}")
     find_library(CSHARP_UNITYEDITOR_LIBRARY UnityEditor.dll PATH ${UNITY_EDITOR_DLL_PATH})
     message("Found: ${CSHARP_UNITYEDITOR_LIBRARY}")
     
     # Standard code to report whether we found the package or not.
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Unity DEFAULT_MSG UNITY_EDITOR_PATH CSHARP_UNITYEDITOR_LIBRARY)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Unity DEFAULT_MSG CSHARP_UNITYEDITOR_LIBRARY)
 else()
-    message("Using ${UNITY_EDITOR_PATH}")
+    message("Using ${UNITY_EDITOR_DLL_PATH}")
 endif()
-
-
