b5734cecbdbaf35587f49bc1902a181e74acacca
vkovec <viktoria.kovecses@gmail.com>
2017-08-04 17:53:34 -0400

    reference UnityEditor.dll

--- deps/cmake/FindUnity.cmake
+++ deps/cmake/FindUnity.cmake
@@ -11,23 +11,38 @@
 # Platform-specific code.
 if (NOT DEFINED UNITY_EDITOR_PATH)
     if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+      SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
+    
       set(UNITY_PATH "/Applications/Unity")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop.app")
+      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Unity.app/Contents/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Unity.app/Contents/MacOS/")
+      
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-      set(UNITY_PATH "c:/Program Files/Unity")
+      SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
+      
+      set(UNITY_PATH "c:/Program Files/Unity2017.1.0f3")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop")
+      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Editor/Data/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Editor/")
+      
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    
       set(UNITY_PATH "/opt/Unity")
       set(MONODEVELOP_PATH "${UNITY_PATH}/MonoDevelop")
+      set(UNITY_EDITOR_DLL_PATH "${UNITY_PATH}/Editor/Data/Managed")
       list(APPEND UNITY_EXECUTABLE_PATHS "${UNITY_PATH}/Editor/")
+      
     endif()
 
     find_program(UNITY_EDITOR_PATH Unity PATHS ${UNITY_EXECUTABLE_PATHS})
 
+    message("Looking for UnityEditor.dll in ${UNITY_EDITOR_DLL_PATH}")
+    find_library(CSHARP_UNITYEDITOR_LIBRARY UnityEditor.dll PATH ${UNITY_EDITOR_DLL_PATH})
+    message("Found: ${CSHARP_UNITYEDITOR_LIBRARY}")
+    
     # Standard code to report whether we found the package or not.
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Unity DEFAULT_MSG UNITY_EDITOR_PATH)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Unity DEFAULT_MSG UNITY_EDITOR_PATH CSHARP_UNITYEDITOR_LIBRARY)
 else()
     message("Using ${UNITY_EDITOR_PATH}")
 endif()
