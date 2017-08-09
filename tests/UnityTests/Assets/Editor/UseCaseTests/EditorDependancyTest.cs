420f1bb80ad777f82363a95d9a12add1915be507
vkovec <viktoria.kovecses@gmail.com>
2017-08-09 15:21:15 -0400

    put editor dependancy test in correct namespace

--- tests/UnityTests/Assets/Editor/UseCaseTests/EditorDependancyTest.cs
+++ tests/UnityTests/Assets/Editor/UseCaseTests/EditorDependancyTest.cs
@@ -14,49 +14,52 @@ using System.Diagnostics;
 using System.Text;
 using System;
 
-public class EditorDependancyTest {
+namespace Unity.FbxSdk.UseCaseTests{
 
-    private string m_exeFileName = null;
-    protected string exeFileName {
-        get {
+    public class EditorDependancyTest {
+
+        private string m_exeFileName = null;
+        protected string exeFileName {
+            get {
 #if UNITY_EDITOR
-            if(m_exeFileName == null){
-                m_exeFileName = Path.Combine (UnityEngine.Application.dataPath, "UnityDependancyTest.exe");
-            }
-            return m_exeFileName;
+                if(m_exeFileName == null){
+                    m_exeFileName = Path.Combine (UnityEngine.Application.dataPath, "UnityDependancyTest.exe");
+                }
+                return m_exeFileName;
 #else
-            throw new NotImplementedException();
+                throw new NotImplementedException();
 #endif
+            }
         }
-    }
 
-    [SetUp]
-    public void Init()
-    {
-        if (!File.Exists (this.exeFileName)) {
-            Assert.Ignore ("Missing exe at " + this.exeFileName);
+        [SetUp]
+        public void Init()
+        {
+            if (!File.Exists (this.exeFileName)) {
+                Assert.Ignore ("Missing exe at " + this.exeFileName);
+            }
         }
-    }
 
-    [Test]
-    public void TestCSharpConsoleApp()
-    {
-        Process consoleApp = new Process ();
-        consoleApp.StartInfo.FileName = this.exeFileName;
-        consoleApp.StartInfo.RedirectStandardError = true;
-        consoleApp.StartInfo.UseShellExecute = false;
-        consoleApp.StartInfo.CreateNoWindow = true;
-        consoleApp.StartInfo.ErrorDialog = false;
-        consoleApp.Start ();
+        [Test]
+        public void TestCSharpConsoleApp()
+        {
+            Process consoleApp = new Process ();
+            consoleApp.StartInfo.FileName = this.exeFileName;
+            consoleApp.StartInfo.RedirectStandardError = true;
+            consoleApp.StartInfo.UseShellExecute = false;
+            consoleApp.StartInfo.CreateNoWindow = true;
+            consoleApp.StartInfo.ErrorDialog = false;
+            consoleApp.Start ();
 
-        StringBuilder error = new StringBuilder ();
-        while (!consoleApp.HasExited) {
-            error.Append (consoleApp.StandardError.ReadToEnd ());
+            StringBuilder error = new StringBuilder ();
+            while (!consoleApp.HasExited) {
+                error.Append (consoleApp.StandardError.ReadToEnd ());
+            }
+            
+            Assert.IsNotEmpty (error.ToString ());
+            Assert.IsTrue (error.ToString ().Contains (
+                "Unhandled Exception: System.TypeInitializationException: " +
+                "The type initializer for 'Unity.FbxSdk.GlobalsPINVOKE' threw an exception"));
         }
-        
-        Assert.IsNotEmpty (error.ToString ());
-        Assert.IsTrue (error.ToString ().Contains (
-            "Unhandled Exception: System.TypeInitializationException: " +
-            "The type initializer for 'Unity.FbxSdk.GlobalsPINVOKE' threw an exception"));
     }
 }
