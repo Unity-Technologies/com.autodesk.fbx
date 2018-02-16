ce0892bcba79c9587c1e7ecb9d24a1b9c9485b2d
Benoit Hudson <benoit.hudson@gmail.com>
2018-02-16 11:33:57 -0500

    Allow building with Unity 2018.1
    When we look for mono on OSX we now look for it both in the Monodevelop.app
    (where it used to be) but also in Unity.app (where is now is, as of 2018.1).
    
    While I was at it I bulletproofed the code to find Unity and to find the
    assemblies.
    
    Added dotnet_find_library to handle the looking for libraries that end in
    .dll on all platforms (though it doesn't seem to work if you don't suffix
    with .dll anyways).
    
    Target for this is to copy these files to the Alembic github repo.

--- deps/cmake/FindCSharpAssemblies.cmake
+++ deps/cmake/FindCSharpAssemblies.cmake
@@ -4,9 +4,11 @@
 # Licensed under the ##LICENSENAME##.
 # See LICENSE.md file in the project root for full license information.
 # ***********************************************************************
+if (CSHARP_ASSEMBLIES_FOUND)
+  return()
+endif()
 
-# We need to have Unity available to compile against it.
-find_package(Unity REQUIRED)
+include(CSharpHelpers)
 
 # The list of .NET versions to look for.
 # The first of these is the most-preferred.
@@ -16,13 +18,26 @@ set(NET_LIB_VERSIONS "2.0.50727" "2.0")
 
 # Platform-specific code.
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-  SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
+  if (DEFINED MONO_ROOT_PATH)
+    message("Using Mono in ${MONO_ROOT_PATH}")
+  else()
+    # If MONO_ROOT_PATH isn't set then look for it in Unity.
+    #
+    # Since Unity 2018.1, Mono is bundled with Unity.app
+    # Before 2018.1 Mono was bundled with Monodevelop.app which was
+    # installed next to Unity.app. Try them both.
+    # UNITY_EDITOR_DLL_PATH is Unity.app/Contents/Managed
+    find_package(Unity REQUIRED)
+    list(APPEND MONO_ROOT_PATH "${UNITY_EDITOR_DLL_PATH}/../Mono")
+    list(APPEND MONO_ROOT_PATH "${UNITY_EDITOR_DLL_PATH}/../../../Monodevelop.app/Contents/Frameworks/Mono.framework/Versions/Current")
+  endif()
 
-  # MONO_ROOT_PATH is from FindUnity (on Darwin)
-  list(APPEND CSHARP_COMPILER_PATHS "${MONO_ROOT_PATH}/bin")
-  foreach(VERSION ${NET_REFERENCE_ASSEMBLIES_VERSIONS})
-      list(APPEND CSHARP_DLL_PATHS "${MONO_ROOT_PATH}/lib/mono/${VERSION}")
-      list(APPEND REFERENCE_ASSEMBLIES_PATHS "${MONO_ROOT_PATH}/lib/mono/${VERSION}")
+  foreach(MONO ${MONO_ROOT_PATH})
+      list(APPEND CSHARP_COMPILER_PATHS "${MONO}/bin")
+      foreach(VERSION ${NET_REFERENCE_ASSEMBLIES_VERSIONS})
+          list(APPEND CSHARP_DLL_PATHS "${MONO}/lib/mono/${VERSION}")
+          list(APPEND REFERENCE_ASSEMBLIES_PATHS "${MONO}/lib/mono/${VERSION}")
+      endforeach()
   endforeach()
 
   find_program(MONO_COMPILER mono PATH ${CSHARP_COMPILER_PATH} NO_DEFAULT_PATH)
@@ -30,7 +45,6 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
 
 elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
   message("Using .Net versions ${NET_COMPILER_VERSIONS}")
-  SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
 
   set(DOT_NET_PATH "C:/Windows/Microsoft.NET/Framework")
   set(REFERENCE_ASSEMBLIES_PATH "C:/Program Files \(x86\)/Reference Assemblies/Microsoft/Framework")
@@ -51,26 +65,24 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   message(WARNING "Linux: Not Implemented")
 endif()
 
-message("Looking for C# compiler in ${CSHARP_COMPILER_PATHS}")
+# message("Looking for C# compiler in ${CSHARP_COMPILER_PATHS}")
 find_program(CSHARP_COMPILER mcs csc PATHS ${CSHARP_COMPILER_PATHS} NO_DEFAULT_PATH)
 find_program(CSHARP_COMPILER mcs csc PATHS ${CSHARP_COMPILER_PATHS})
-message("Found: ${CSHARP_COMPILER}\n")
 
-message("Looking for mscorlib.dll in ${CSHARP_DLL_PATHS}")
-find_library(CSHARP_MSCORLIB_LIBRARY mscorlib.dll PATHS ${CSHARP_DLL_PATHS} NO_DEFAULT_PATH)
-find_library(CSHARP_MSCORLIB_LIBRARY mscorlib.dll PATHS ${CSHARP_DLL_PATHS})
-message("Found: ${CSHARP_MSCORLIB_LIBRARY}\n")
+# message("Looking for mscorlib.dll in ${CSHARP_DLL_PATHS}")
+dotnet_find_library(CSHARP_MSCORLIB_LIBRARY mscorlib.dll PATHS ${CSHARP_DLL_PATHS} NO_DEFAULT_PATH)
+dotnet_find_library(CSHARP_MSCORLIB_LIBRARY mscorlib.dll PATHS ${CSHARP_DLL_PATHS})
+dotnet_find_library(CSHARP_SYSTEM_LIBRARY System.dll PATHS ${CSHARP_DLL_PATHS} NO_DEFAULT_PATH)
+dotnet_find_library(CSHARP_SYSTEM_LIBRARY System.dll PATHS ${CSHARP_DLL_PATHS})
 
-message("Looking for System.dll in ${CSHARP_DLL_PATHS}")
-find_library(CSHARP_SYSTEM_LIBRARY System.dll PATHS ${CSHARP_DLL_PATHS} NO_DEFAULT_PATH)
-find_library(CSHARP_SYSTEM_LIBRARY System.dll PATHS ${CSHARP_DLL_PATHS})
-message("Found: ${CSHARP_SYSTEM_LIBRARY}\n")
-
-message("Looking for System.Core.dll in ${REFERENCE_ASSEMBLIES_PATHS}")
-find_library(CSHARP_SYSTEM_CORE_LIBRARY System.Core.dll PATHS ${REFERENCE_ASSEMBLIES_PATHS} NO_DEFAULT_PATH)
-find_library(CSHARP_SYSTEM_CORE_LIBRARY System.Core.dll PATHS ${REFERENCE_ASSEMBLIES_PATHS})
-message("Found: ${CSHARP_SYSTEM_CORE_LIBRARY}\n")
+# message("Looking for System.Core.dll in ${REFERENCE_ASSEMBLIES_PATHS}")
+dotnet_find_library(CSHARP_SYSTEM_CORE_LIBRARY System.Core.dll PATHS ${REFERENCE_ASSEMBLIES_PATHS} NO_DEFAULT_PATH)
+dotnet_find_library(CSHARP_SYSTEM_CORE_LIBRARY System.Core.dll PATHS ${REFERENCE_ASSEMBLIES_PATHS})
 
 # Standard code to report whether we found the package or not.
 #include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CSHARP_ASSEMBLIES DEFAULT_MSG CSHARP_COMPILER CSHARP_MSCORLIB_LIBRARY CSHARP_SYSTEM_LIBRARY CSHARP_SYSTEM_CORE_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CSHARP_ASSEMBLIES DEFAULT_MSG
+        CSHARP_COMPILER
+        CSHARP_MSCORLIB_LIBRARY
+        CSHARP_SYSTEM_LIBRARY
+        CSHARP_SYSTEM_CORE_LIBRARY)
