2e1b2fc7bac8a2263902fc3d53040c59dba7abbf
vkovec <viktoria.kovecses@gmail.com>
2018-04-04 14:36:31 -0400

    add missing unit tests
    + refactor so code is reused

--- tests/UnityTests/Assets/Editor/UnitTests/FbxConstraintTest.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/FbxConstraintTest.cs
@@ -30,9 +30,88 @@ namespace Unity.FbxSdk.UnitTests
 		}
 	}
 
+	/// <summary>
+	/// For testing functions that classes that derive from FbxConstraint share, but are not implemented in FbxConstraint.
+	/// </summary>
+	public abstract class FbxConstraintDescendantTestBase<T> : FbxConstraintTestBase<T> where T : FbxConstraint{
+		static System.Reflection.MethodInfo s_AddConstraintSource;
+		static System.Reflection.MethodInfo s_AddConstraintSourceDouble;
+		static System.Reflection.MethodInfo s_SetConstrainedObject;
+
+		static FbxConstraintDescendantTestBase() {
+			s_AddConstraintSource = typeof(T).GetMethod("AddConstraintSource", new System.Type[] { typeof(FbxObject) });
+			s_AddConstraintSourceDouble = typeof(T).GetMethod("AddConstraintSource", new System.Type[] { typeof(FbxObject), typeof(double) });
+			s_SetConstrainedObject = typeof(T).GetMethod ("SetConstrainedObject", new System.Type[] { typeof(FbxObject) });
+
+			#if ENABLE_COVERAGE_TEST
+			// Register the calls we make through reflection.
+			if(s_AddConstraintSource != null){
+				var addConstraintSource = typeof(FbxConstraintDescendantTestBase<T>).GetMethod("AddConstraintSource");
+				CoverageTester.RegisterReflectionCall(addConstraintSource, s_AddConstraintSource);
+			}
+			if(s_AddConstraintSourceDouble != null){
+				var addConstraintSourceDouble = typeof(FbxConstraintDescendantTestBase<T>).GetMethod("AddConstraintSourceDouble");
+				CoverageTester.RegisterReflectionCall(addConstraintSourceDouble, s_AddConstraintSourceDouble);
+			}
+			if(s_SetConstrainedObject != null){
+				var setConstrainedObject = typeof(FbxConstraintDescendantTestBase<T>).GetMethod("SetConstrainedObject");
+				CoverageTester.RegisterReflectionCall(setConstrainedObject, s_SetConstrainedObject);
+			}
+			#endif
+		}
+
+		public void AddConstraintSourceDouble(T instance, FbxObject obj, double weight){
+			Invoker.Invoke (s_AddConstraintSourceDouble, instance, obj, weight);
+		}
+
+		public void AddConstraintSource(T instance, FbxObject obj){
+			Invoker.Invoke (s_AddConstraintSource, instance, obj);
+		}
+
+		public void SetConstrainedObject(T instance, FbxObject obj){
+			Invoker.Invoke (s_SetConstrainedObject, instance, obj);
+		}
+
+		[Test]
+		public virtual void TestAddConstraintSource(){
+			using (var constraint = CreateObject("constraint")) {
+				Assert.That(() => AddConstraintSource (constraint, null), Throws.Exception.TypeOf<System.NullReferenceException>());
+				Assert.That (constraint.GetConstraintSourceCount(), Is.EqualTo(0));
+
+				var fbxNode = FbxNode.Create (Manager, "rootnode");
+
+				AddConstraintSource (constraint, fbxNode);
+				Assert.That (constraint.GetConstraintSource (0), Is.EqualTo (fbxNode));
+				Assert.That (constraint.GetConstraintSourceCount (), Is.EqualTo (1));
+
+				fbxNode = FbxNode.Create (Manager, "node2");
+				AddConstraintSourceDouble (constraint, fbxNode, 2.0);
+				Assert.That (constraint.GetConstraintSource (1), Is.EqualTo (fbxNode));
+				Assert.That (constraint.GetConstraintSourceCount (), Is.EqualTo (2));
+			}
+		}
+
+		[Test]
+		public virtual void TestSetConstrainedObject(){
+			if (ConstraintType == FbxConstraint.EType.eUnknown) {
+				return;
+			}
+
+			using (var constraint = CreateObject("constraint")) {
+				Assert.That(() => SetConstrainedObject (constraint, null), Throws.Exception.TypeOf<System.NullReferenceException>());
+
+				var fbxNode = FbxNode.Create (Manager, "rootnode");
+
+				SetConstrainedObject (constraint, fbxNode);
+				Assert.That (constraint.GetConstrainedObject (), Is.EqualTo (fbxNode));
+			}
+		}
+	}
+
+
 	public class FbxConstraintTest : FbxConstraintTestBase<FbxConstraint>{}
 
-	public class FbxConstraintAimTest : FbxConstraintTestBase<FbxConstraintAim>
+	public class FbxConstraintAimTest : FbxConstraintDescendantTestBase<FbxConstraintAim>
 	{
 		protected override FbxConstraint.EType ConstraintType {
 			get {
@@ -55,8 +134,63 @@ namespace Unity.FbxSdk.UnitTests
 		}
 
 		[Test]
-		public void TestAddConstraintSource(){
+		public void TestWorldUpObject(){
 			using (var constraint = FbxConstraintAim.Create (Manager, "aimConstraint")) {
+				Assert.That(() => constraint.SetWorldUpObject (null), Throws.Exception.TypeOf<System.NullReferenceException>());
+
+				var fbxNode = FbxNode.Create (Manager, "rootnode");
+
+				constraint.SetWorldUpObject (fbxNode);
+				Assert.That (constraint.GetWorldUpObject (), Is.EqualTo (fbxNode));
+			}
+		}
+	}
+
+	public class FbxConstraintParentTest : FbxConstraintDescendantTestBase<FbxConstraintParent>{
+		protected override FbxConstraint.EType ConstraintType {
+			get {
+				return FbxConstraint.EType.eParent;
+			}
+		}
+
+		[Test]
+		public void TestGetters(){
+			using (var constraint = FbxConstraintParent.Create (Manager, "pConstraint")) {
+				TestGetter (constraint.AffectRotationX);
+				TestGetter (constraint.AffectRotationY);
+				TestGetter (constraint.AffectRotationZ);
+				TestGetter (constraint.AffectScalingX);
+				TestGetter (constraint.AffectScalingY);
+				TestGetter (constraint.AffectScalingZ);
+				TestGetter (constraint.AffectTranslationX);
+				TestGetter (constraint.AffectTranslationY);
+				TestGetter (constraint.AffectTranslationZ);
+			}
+		}
+	}
+
+	public class FbxConstraintPositionTest : FbxConstraintDescendantTestBase<FbxConstraintPosition> {
+		protected override FbxConstraint.EType ConstraintType {
+			get {
+				return FbxConstraint.EType.ePosition;
+			}
+		}
+
+		[Test]
+		public void TestGetters(){
+			using (var constraint = FbxConstraintPosition.Create (Manager, "posConstraint")) {
+				TestGetter (constraint.AffectX);
+				TestGetter (constraint.AffectY);
+				TestGetter (constraint.AffectZ);
+				TestGetter (constraint.Translation);
+			}
+		}
+
+		[Test]
+		public override void TestAddConstraintSource(){
+			// overriding implementation because FbxConstraintPosition also has a RemoveConstraintSource() function
+
+			using (var constraint = FbxConstraintPosition.Create (Manager, "pConstraint")) {
 				Assert.That(() => constraint.AddConstraintSource (null), Throws.Exception.TypeOf<System.NullReferenceException>());
 				Assert.That (constraint.GetConstraintSourceCount(), Is.EqualTo(0));
 
@@ -66,34 +200,52 @@ namespace Unity.FbxSdk.UnitTests
 				Assert.That (constraint.GetConstraintSource (0), Is.EqualTo (fbxNode));
 				Assert.That (constraint.GetConstraintSourceCount (), Is.EqualTo (1));
 
-				fbxNode = FbxNode.Create (Manager, "node2");
-				constraint.AddConstraintSource (fbxNode, 2);
-				Assert.That (constraint.GetConstraintSource (1), Is.EqualTo (fbxNode));
+				var fbxNode2 = FbxNode.Create (Manager, "node2");
+				constraint.AddConstraintSource (fbxNode2, 2);
+				Assert.That (constraint.GetConstraintSource (1), Is.EqualTo (fbxNode2));
 				Assert.That (constraint.GetConstraintSourceCount (), Is.EqualTo (2));
+
+				Assert.That(() => constraint.RemoveConstraintSource (null), Throws.Exception.TypeOf<System.NullReferenceException>());
+
+				constraint.RemoveConstraintSource (fbxNode);
+				Assert.That (constraint.GetConstraintSourceCount (), Is.EqualTo (1));
+				Assert.That (constraint.GetConstraintSource (0), Is.EqualTo (fbxNode2));
+			}
+		}
+	}
+
+	public class FbxConstraintRotationTest : FbxConstraintDescendantTestBase<FbxConstraintRotation>{
+		protected override FbxConstraint.EType ConstraintType {
+			get {
+				return FbxConstraint.EType.eRotation;
 			}
 		}
 
 		[Test]
-		public void TestSetConstrainedObject(){
-			using (var constraint = FbxConstraintAim.Create (Manager, "aimConstraint")) {
-				Assert.That(() => constraint.SetConstrainedObject (null), Throws.Exception.TypeOf<System.NullReferenceException>());
+		public void TestGetters(){
+			using (var constraint = FbxConstraintRotation.Create (Manager, "rConstraint")) {
+				TestGetter (constraint.AffectX);
+				TestGetter (constraint.AffectY);
+				TestGetter (constraint.AffectZ);
+				TestGetter (constraint.Rotation);
+			}
+		}
+	}
 
-				var fbxNode = FbxNode.Create (Manager, "rootnode");
-
-				constraint.SetConstrainedObject (fbxNode);
-				Assert.That (constraint.GetConstrainedObject (), Is.EqualTo (fbxNode));
+	public class FbxConstraintScaleTest : FbxConstraintDescendantTestBase<FbxConstraintScale>{
+		protected override FbxConstraint.EType ConstraintType {
+			get {
+				return FbxConstraint.EType.eScale;
 			}
 		}
 
 		[Test]
-		public void TestWorldUpObject(){
-			using (var constraint = FbxConstraintAim.Create (Manager, "aimConstraint")) {
-				Assert.That(() => constraint.SetWorldUpObject (null), Throws.Exception.TypeOf<System.NullReferenceException>());
-
-				var fbxNode = FbxNode.Create (Manager, "rootnode");
-
-				constraint.SetWorldUpObject (fbxNode);
-				Assert.That (constraint.GetWorldUpObject (), Is.EqualTo (fbxNode));
+		public void TestGetters(){
+			using (var constraint = FbxConstraintScale.Create (Manager, "sConstraint")) {
+				TestGetter (constraint.AffectX);
+				TestGetter (constraint.AffectY);
+				TestGetter (constraint.AffectZ);
+				TestGetter (constraint.Scaling);
 			}
 		}
 	}
