cded0db33abc1e4eeb50ba8e807beab374ccde62
vkovec <viktoria.kovecses@gmail.com>
2018-04-10 15:44:25 -0400

    add missing invoker functions

--- tests/UnityTests/Assets/Editor/UnitTests/Invoker.cs
+++ tests/UnityTests/Assets/Editor/UnitTests/Invoker.cs
@@ -63,6 +63,36 @@ public static class Invoker
     }
 
     /**
+     * Invoke a single-argument instance method with no return value.
+     */
+    public static void Invoke(System.Reflection.MethodInfo method, object instance, object arg)
+    {
+        try
+        {
+            method.Invoke(instance, new object[] { arg });
+        }
+        catch (System.Reflection.TargetInvocationException xcp)
+        {
+            throw xcp.GetBaseException();
+        }
+    }
+
+    /**
+     * Invoke a two-argument instance method with no return value.
+     */
+    public static void Invoke(System.Reflection.MethodInfo method, object instance, object arg1, object arg2)
+    {
+        try
+        {
+            method.Invoke(instance, new object[] { arg1, arg2 });
+        }
+        catch (System.Reflection.TargetInvocationException xcp)
+        {
+            throw xcp.GetBaseException();
+        }
+    }
+
+    /**
      * Invoke a single-argument static method.
      */
     public static U InvokeStatic<U>(System.Reflection.MethodInfo method, object arg) {
