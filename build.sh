1004b37ff945944f2a9835500ad1d8def4060ec2
Tak <levi@unity3d.com>
2018-11-13 10:42:03 +0100

    Make build work on linux

--- build.sh
+++ build.sh
@@ -9,7 +9,13 @@ mkdir -p build
 pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${installdir}
+
 # can't find another way to tell SWIG where to find its libraries
-export SWIG_LIB=${PWD}/deps/swig-mac-x64/share/swig/3.0.12
+SWIG_VERSION="3.0.12"
+if test x`uname -s` = xDarwin; then
+	export SWIG_LIB=${PWD}/deps/swig-mac-x64/share/swig/${SWIG_VERSION}
+else
+	export SWIG_LIB=${PWD}/deps/swig-linux-x64/share/swig/${SWIG_VERSION}
+fi
 cmake --build . --target install --config Release
 popd
