58dacd8d356eadb65e3318b085bb498682e82048
Mark Visser <markv@unity3d.com>
2019-04-12 15:12:39 -0400

    fixed path to perf tests binary

--- proto.com.autodesk.fbx/Tests/Editor/PerformanceTests/PerformanceTest.cs
+++ proto.com.autodesk.fbx/Tests/Editor/PerformanceTests/PerformanceTest.cs
@@ -21,11 +21,11 @@ namespace Autodesk.Fbx.PerformanceTests
         protected string exeFileName {
             get {
 #if UNITY_EDITOR_WIN
-                return Path.GetFullPath("Packages/com.autodesk.fbx.tests/PerformanceBenchmarks-win-x64.exe");
+                return Path.GetFullPath("Packages/com.autodesk.fbx/Tests/Bin~/PerformanceBenchmarks-win-x64.exe");
 #elif UNITY_EDITOR_OSX
-                return Path.GetFullPath("Packages/com.autodesk.fbx.tests/PerformanceBenchmarks-mac-x64");
+                return Path.GetFullPath("Packages/com.autodesk.fbx/Tests/Bin~/PerformanceBenchmarks-mac-x64");
 #elif UNITY_EDITOR_LINUX
-                return Path.GetFullPath("Packages/com.autodesk.fbx.tests/PerformanceBenchmarks-linux-x64");
+                return Path.GetFullPath("Packages/com.autodesk.fbx/Tests/Bin~/PerformanceBenchmarks-linux-x64");
 #else
                 throw new NotImplementedException();
 #endif
