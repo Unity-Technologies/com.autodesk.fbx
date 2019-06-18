c94a2ab2e55562cfd66a5c5166dc9f18809620e4
Mark Visser <markv@unity3d.com>
2019-06-18 13:37:32 -0400

    added linux build & test (#289)
    * added linux build & test
    * fixed paths
    * updated bee.exe; force verbose build
    * removed MONO_CONFIG from stevedore on linux, was causing failure
    * fixed typo in test vm type
    * use template for build jobs, added build artifacts to test jobs
    * added build dependencies to pack job
    * added missing template variable build_command
    * restored linux .so.meta for plugin
    * only enable performance tests on Windows; Yamato doesn't preserve the executable attribute so tests fail on *nix

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ fi
 installdir=${PWD}/build/install
 mkdir -p build
 pushd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${installdir}
+VERBOSE=1 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${installdir}
 
 # can't find another way to tell SWIG where to find its libraries
 SWIG_VERSION="3.0.12"
@@ -17,5 +16,5 @@ if test x`uname -s` = xDarwin; then
 else
 	export SWIG_LIB=${PWD}/deps/swig-linux-x64/share/swig/${SWIG_VERSION}
 fi
-cmake --build . --target install --config Release
+VERFBOSE=1 cmake --build . --target install --config Release 
 popd
