41139eb84674ef4b31c52ceccc813ae48eeffbd0
vkovec <viktoria.kovecses@gmail.com>
2020-10-06 15:25:25 -0400

    always use cmake instead of cmake3

--- build.py
+++ build.py
@@ -32,14 +32,7 @@ if sys.platform.startswith('win'):
     shell = True
     cmake_exe = 'cmake.exe'
 else:
-    # check if we should use cmake or cmake3
-    cmake_version = subprocess.check_output(["cmake", "--version"], shell=False)
-    # result will be either "cmake version ...." or "cmake3 version ...."
-    if cmake_version.startswith(b'cmake3'):
-        # cmake is CMake2 on centos, need to be explicit.
-        cmake_exe = 'cmake3'
-    else:
-        cmake_exe = 'cmake'
+    cmake_exe = 'cmake'
     shell = False
 
 # Minimal configuration
