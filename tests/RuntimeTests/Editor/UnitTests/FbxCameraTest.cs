f6bd458fa2c69ce239f4dae5445763630b78f28a
vkovec <viktoria.kovecses@imaginary-spaces.com>
2022-10-31 14:53:48 -0400

    add unit tests

--- tests/RuntimeTests/Editor/UnitTests/FbxCameraTest.cs
+++ tests/RuntimeTests/Editor/UnitTests/FbxCameraTest.cs
@@ -79,6 +79,8 @@ namespace Autodesk.Fbx.UnitTests
                 TestGetter (fbxCamera.GateFit);
                 TestGetter (fbxCamera.FilmOffsetX);
                 TestGetter (fbxCamera.FilmOffsetY);
+                TestGetter (fbxCamera.FocusDistance);
+                TestGetter (fbxCamera.UseDepthOfField);
             }
         }
     }
