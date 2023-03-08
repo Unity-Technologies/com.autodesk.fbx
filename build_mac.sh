6e358edaeff5ef9bdb346531ad2f53386e179516
wind.xu <wind.xu@unity3d.com>
2023-03-08 15:46:33 -0500

    Add build scripts for mac and Linux

---
+++ build_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Any subsequent(*) commands which fail will cause the shell script to exit immediately, otherwise the job will fail silently.
+set -e
+
+brew list p7zip || brew install p7zip
+cmake --version || brew install cmake
+python ./build.py --stevedore --verbose --clean --yamato
+mv build build-mac
