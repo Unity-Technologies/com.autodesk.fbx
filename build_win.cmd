24c8848d259cf1d46fb3df963ea4c99e3c78186c
wind.xu <wind.xu@unity3d.com>
2024-02-27 19:15:57 -0500

    Update build script for windows to test local fbxsdk

--- build_win.cmd
+++ build_win.cmd
@@ -1,3 +1,3 @@
 choco -v -y install 7zip
-py -3 build.py --stevedore --verbose --clean --yamato
+py -3 build.py --stevedore --verbose --clean --yamato --fbxsdk "fbxsdk_to_upload"
 ren build build-win
