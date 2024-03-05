7a4c17f9579399b39dfab78d85b5ae50f50ed3c4
wind.xu <wind.xu@unity3d.com>
2024-03-05 14:03:31 -0500

    Update artifacts id in CMakeLists.txt and build scripts

--- build_win.cmd
+++ build_win.cmd
@@ -1,3 +1,7 @@
 choco -v -y install 7zip
-py -3 build.py --stevedore --verbose --clean --yamato --fbxsdk "fbxsdk_to_upload"
+py -3 build.py --stevedore --verbose --clean --yamato
+IF %ERRORLEVEL% NEQ 0 (
+    echo Build command failed on Windows with error code: %ERRORLEVEL%
+    exit %ERRORLEVEL%
+)
 ren build build-win
