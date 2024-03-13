1c104c9179b23aa972acbd6516ebc0231edfa0e0
wind.xu <wind.xu@unity3d.com>
2024-03-13 10:06:19 -0400

    Remove lines related to gcc-9 since we are already on Ubuntu 20 now

--- build_linux.sh
+++ build_linux.sh
@@ -8,9 +8,6 @@ sudo apt-get update
 sudo apt-get -y install zlib1g-dev
 sudo apt-get -y install cmake
 sudo apt-get -y install libxml2-dev
-sudo apt-get -y install gcc-9 g++-9
 sudo apt-get -y install p7zip mono-devel
-# Ensure correct version of gcc and g++ used
-# https://stackoverflow.com/questions/17275348/how-to-specify-new-gcc-path-for-cmake
-CC=`which gcc-9` CXX=`which g++-9` python ./build.py --stevedore --verbose --clean --yamato
+python ./build.py --stevedore --verbose --clean --yamato
 mv build build-ubuntu
