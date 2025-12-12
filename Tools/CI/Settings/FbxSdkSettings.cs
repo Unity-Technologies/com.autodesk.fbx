03bda64aac24df72c249ac34d31f2265843a8cec
wind.xu <wind.xu@unity3d.com>
2025-12-12 12:53:35 -0500

    Add Wrench template

---
+++ Tools/CI/Settings/FbxSdkSettings.cs
@@ -0,0 +1,31 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace FbxSdk.Cookbook.Settings;
+
+public class FbxSdkSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"./com.autodesk.fbx"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.autodesk.fbx",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public FbxSdkSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions,
+            wrenchCsProjectPath: "Tools/CI/FbxSdk.Cookbook.csproj"
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
